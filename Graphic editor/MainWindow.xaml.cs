78b1582d43220238f14964e1ec44cd8f3ce9fc4f
Roman <3romanski@gmail.com>
2019-09-25 19:38:03 +0300

    Rhomb just appears on the place where you click

---
+++ Graphic editor/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Graphic_editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Canvas.SetLeft(Fcanvas, p.X);
+            Canvas.SetTop(Fcanvas, p.Y);
+            label.Content = p.X.ToString() + " " + p.Y.ToString();
+        }
+
+        private void Canvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            //label.Content = p.X.ToString() + " " + p.Y.ToString();
+        }
+    }
+
+    
+}
