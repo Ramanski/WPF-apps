46a93fc23bf2154f74280ab318784453306b0532
Roman <3romanski@gmail.com>
2019-10-13 15:25:26 +0300

    organizational renaming of files

--- MovingButton/lab2/MainWindow.xaml.cs
+++ MovingButton/lab2/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Drawing;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 using Point = System.Windows.Point;
 
 namespace lab2
@@ -16,69 +12,64 @@ namespace lab2
     {
         double CONTAINER_RIGHT_BORDER;
         double CONTAINER_BOTTOM_BORDER;
-        double BUTTON_NO_WIDTH;
-        double BUTTON_NO_HEIGHT;
-        Storyboard sb = new Storyboard();
+        double X;
+        double Y;
 
         public MainWindow()
         {
             InitializeComponent();
-            CONTAINER_RIGHT_BORDER = canvas.Width;
-            CONTAINER_BOTTOM_BORDER = canvas.Height;
-            BUTTON_NO_WIDTH = but_no.Width;
-            BUTTON_NO_HEIGHT = but_no.Height;
-
-            
-
-            DoubleAnimation animA = new DoubleAnimation()
-            {
-                From = 0,
-                To = 120,
-                Duration = System.TimeSpan.FromSeconds(1),
-                AutoReverse = true,
-            };
-            Storyboard.SetTargetProperty(animA, new PropertyPath("(Button.RenderTransform).(TranslateTransform.X)"));
-            sb.Children.Add(animA);
-            DoubleAnimation animB = new DoubleAnimation()
-            {
-                From = 0,
-                To = 120,
-                Duration = System.TimeSpan.FromSeconds(0.5),
-                AutoReverse = true,
-            };
-
-            Storyboard.SetTargetProperty(animB, new PropertyPath("(Button.RenderTransform).(TranslateTransform.Y)"));
-            sb.Children.Add(animB);
+            CONTAINER_RIGHT_BORDER = win.Width;
+            CONTAINER_BOTTOM_BORDER = win.Height;            
+            X = Canvas.GetLeft(but_no);
+            Y = Canvas.GetTop(but_no);
         }
-        
-        private void No_MouseEnter(object sender, MouseEventArgs e)
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            sb.Begin(but_no);
-            but_no.FontWeight = new FontWeight();
-            DoubleAnimation v = new DoubleAnimation(30, 80, TimeSpan.FromSeconds(0.5)) { AutoReverse = true };
-            but_no.BeginAnimation(Button.HeightProperty, v);
+            if (X < 0)
+            {
+                X = but_no.Width + 30;
+                Canvas.SetLeft(but_no, X);
+            }
+            if (Y < 0)
+            {
+                Y = but_no.Height + 30;
+                Canvas.SetTop(but_no, Y);
+            }
+            if (X + but_no.Width > CONTAINER_RIGHT_BORDER)
+            {
+                X = CONTAINER_RIGHT_BORDER - but_no.Width;
+                Canvas.SetLeft(but_no, X);
+            }
+            if (Y + but_no.Height > CONTAINER_BOTTOM_BORDER)
+            {
+                Y = CONTAINER_BOTTOM_BORDER - but_no.Height;
+                Canvas.SetTop(but_no, Y);
+            }
         }
-            private void Canvas_MouseMove(object sender, MouseEventArgs e)
-        {
-            
-            /*
-            Thickness margin = but_no.Margin;
-            Point p = e.GetPosition(this);
-            //Point locationFromScreen = canvas.but_no.PointToScreen(new Point(0, 0));
-            // if (Canvas.GetLeft(but_no) == p.X) Canvas.SetLeft(but_no, -1);
-           // label1.Content = Canvas.GetLeft(but_no).ToString();
-            if ((margin.Left ) == p.X) margin.Left++;
-            if ((margin.Top) == p.Y) margin.Top++;
-            if (margin.Left + BUTTON_NO_WIDTH == p.X) margin.Left--;
-            if (margin.Top + BUTTON_NO_HEIGHT == p.Y) margin.Top--;
-            but_no.Margin = margin;  
-            */
-        }
-        
+
         private void But_yes_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Молодца");
+            MessageBox.Show("Молодца","Ответ системы");
             Application.Current.Shutdown();
+        }
+
+        private void Win_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CONTAINER_RIGHT_BORDER = win.Width;
+            CONTAINER_BOTTOM_BORDER = win.Height;
+        }
+
+        private void No_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(canvas);
+            if ((p.X > X - 15) && (p.X < X + 115) && (p.Y > Y - 15) && (p.Y < Y + 51))
+            {
+                if (p.X < X + but_no.Width/2) Canvas.SetLeft(but_no, X++);
+                    else Canvas.SetLeft(but_no, X--);
+                if (p.Y < Y + but_no.Height/2) Canvas.SetTop(but_no, Y++);
+                    else Canvas.SetTop(but_no, Y--);
+            }
         }
     }
 }
